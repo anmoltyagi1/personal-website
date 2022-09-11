b677b94b08ca6a0d66733249d380800a18383c64
Anmol Tyagi <74927598+anmoltyagi1@users.noreply.github.com>
2022-09-11 10:26:42 -0400

    changed image path

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
-import profilePic from "../holly.png";
+import profilePic from "../pp1.png";
 import styled from "styled-components";
 import Nav from "../components/Nav";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
